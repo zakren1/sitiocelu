b1d0307227eca183b78df266ddcbcad59c3e1750
zakren1 <zakren1234@gmail.com>
2024-05-28 22:45:58 -0400

    plantillas listas about, base, carrito, detallepedidouser, index, inicioseion, iphone, pedidouser, perfilusuario, recuperarcontra, registrouser, samsung, xiaomi

--- mobilemart/urls.py
+++ mobilemart/urls.py
@@ -1,8 +1,21 @@
 
 from django.urls import include, path
 
-from .views import index
+from .views import index, iphone, samsung, xiaomi, administracion, carrito, perfilusuario, pedidosuser,InicioSesion,about,detalle_producto,recuperar_contrasena,registro_usuario,detalle_pedido_usuario
 
 urlpatterns = [
     path('',index,name='index'),
+    path('iphone/', iphone, name='iphone'),
+    path('samsung/', samsung, name='samsung'),
+    path('xiaomi/', xiaomi, name='xiaomi'),
+    path('administracion/', administracion, name='administracion'),
+    path('carrito/', carrito, name='carrito'),
+    path('perfilusuario/', perfilusuario, name='perfilusuario'),
+    path('pedidosuser/', pedidosuser, name='pedidosuser'),
+    path('InicioSesion/', InicioSesion, name='InicioSesion'),
+    path('about/', about, name='about'),
+    path('detalle_producto', detalle_producto, name='detalle_producto'),
+    path('recuperarcontra/', recuperar_contrasena, name='recuperar_contrasena'),
+    path('registro/', registro_usuario, name='registro_usuario'),
+    path('detallepedidouser/', detalle_pedido_usuario, name='detalle_pedido_usuario'),
 ]
--- mobilemart/views.py
+++ mobilemart/views.py
@@ -3,3 +3,56 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'mobilemart/index.html')
+
+
+# Vista para la página de Apple
+def iphone(request):
+    return render(request, 'mobilemart/iphone.html')
+
+# Vista para la página de Samsung
+def samsung(request):
+    return render(request, 'mobilemart/samsung.html')
+
+# Vista para la página de Xiaomi
+def xiaomi(request):
+    return render(request, 'mobilemart/xiaomi.html')
+
+# Vista para la página de Administración
+def administracion(request):
+    return render(request, 'mobilemart/administracion.html')
+
+# Vista para la página del Carrito
+def carrito(request):
+    return render(request, 'mobilemart/carrito.html')
+
+# Vista para la página del Perfil del Usuario
+def perfilusuario(request):
+    return render(request, 'mobilemart/perfilusuario.html')
+
+# Vista para la página de Pedidos del Usuario
+def pedidosuser(request):
+    return render(request, 'mobilemart/pedidosuser.html')
+
+# Vista para la página de Inicio de Sesión
+def InicioSesion(request):
+    return render(request, 'mobilemart/InicioSesion.html')
+
+# Vista para la página Acerca de
+def about(request):
+    return render(request, 'mobilemart/about.html')
+
+# Vista para la página detalle producto
+def detalle_producto(request):
+    return render(request, 'mobilemart/iphone15pro.html')
+
+# Vista para la página recuperar contraseña
+def recuperar_contrasena(request):
+    return render(request, 'mobilemart/recuperarcontra.html')
+
+# Vista para la página registro usuario
+def registro_usuario(request):
+    return render(request, 'mobilemart/RegistroUser.html')
+
+# Vista para la página detalle pedido usuario
+def detalle_pedido_usuario(request):
+    return render(request, 'mobilemart/detallepedidouser.html')
