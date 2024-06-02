ea68ebc5d02fd73563ce2c2a5659e819588d51ad
m-fuentesr <maj.fuentes@duocuc.cl>
2024-06-02 19:35:06 -0400

    Agregados models

--- mobilemart/admin.py
+++ mobilemart/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Usuario
+from .models import Celular
+
+class AdmUsuario(admin.ModelAdmin):
+    list_display= ['rut', 'username', 'correo']
+    #list_editable= ['rut', 'username', 'correo']
+
+class AdmCelular(admin.ModelAdmin):
+    list_display=['marca', 'modelo', 'precio', 'foto'] #El id lo creó Django autom. por que Mascota no tiene clave primaria
+    list_editable=['precio', 'foto']
+    list_filter=['marca']
+
+
+
 
 # Register your models here.
+admin.site.register(Usuario, AdmUsuario)
+admin.site.register(Celular, AdmCelular)
+
